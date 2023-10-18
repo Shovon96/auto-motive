a49d31bc175b3a1ad2898f3930d47a0937beaad9
Shovon96 <fakhruddinshovon@gmail.com>
2023-10-18 06:42:27 +0600

    packeges section added

---
+++ src/Components/OurPackege/OurPackeges.jsx
@@ -0,0 +1,63 @@
+
+const OurPackeges = () => {
+    return (
+        <div className="max-w-6xl mx-auto">
+            <div className="mt-8 text-center">
+                <h1 className="text-5xl font-bold"><span className="text-rose-600">Price</span> Table</h1>
+                <p className="text-lg text-gray-500 my-2">Rutrum ante tempus mauris facilisi, leo faucibus, <br />
+                    egestas diamlorem malesu, vitae mauris.
+                </p>
+                <hr />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-12 gap-5">
+                <div
+                    data-aos="zoom-in-down"
+                    data-aos-duration="2000"
+                    className="text-center shadow-md rounded-md">
+                    <h4 className="text-2xl font-bold mt-2">One-Time</h4>
+                    <p className="text-xl my-2 font-semibold">Selection of curs</p>
+                    <h1><span className="text-3xl font-bold text-rose-500">$</span> <span className="text-6xl font-extrabold text-rose-500">100</span><span className="text-gray-400"> /per.mounth</span></h1>
+                    <p className="text-xl text-gray-500">Departure of a specialist <br />
+                        Inspection report <br />
+                        Body Inspection <br />
+                        Computer diagnostics <br />
+                        Inspection of salon electricians
+                    </p>
+                    <button className="btn bg-rose-600 px-8 text-white textarea-bordered my-3 hover:bg-rose-700">Booking</button>
+                </div>
+                <div
+                    data-aos="zoom-in-down"
+                    data-aos-duration="2000"
+                    className="text-center shadow-2xl shadow-gray-600 rounded-md">
+                    <h4 className="text-2xl font-bold mt-2">One-Time</h4>
+                    <p className="text-xl my-2 font-semibold">Selection of curs</p>
+                    <h1><span className="text-3xl font-bold text-rose-500">$</span> <span className="text-6xl font-extrabold text-rose-500">300</span><span className="text-gray-400"> /per.mounth</span></h1>
+                    <p className="text-xl text-gray-500">Departure of a specialist <br />
+                        Inspection report <br />
+                        Body Inspection <br />
+                        Computer diagnostics <br />
+                        Inspection of salon electricians
+                    </p>
+                    <button className="btn bg-rose-600 px-8 text-white textarea-bordered my-3 hover:bg-rose-700">Booking</button>
+                </div>
+                <div
+                    data-aos="zoom-in-down"
+                    data-aos-duration="2000"
+                    className="text-center shadow-md rounded-md">
+                    <h4 className="text-2xl font-bold mt-2">One-Time</h4>
+                    <p className="text-xl my-2 font-semibold">Selection of curs</p>
+                    <h1><span className="text-3xl font-bold text-rose-500">$</span> <span className="text-6xl font-extrabold text-rose-500">200</span><span className="text-gray-400"> /per.mounth</span></h1>
+                    <p className="text-xl text-gray-500">Departure of a specialist <br />
+                        Inspection report <br />
+                        Body Inspection <br />
+                        Computer diagnostics <br />
+                        Inspection of salon electricians
+                    </p>
+                    <button className="btn bg-rose-600 px-8 text-white textarea-bordered my-3 hover:bg-rose-700">Booking</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default OurPackeges;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../Components/Banner/Banner";
 import Brands from "../../Components/BrandsName/Brands";
 import Aos from "aos";
 import 'aos/dist/aos.css';
+import OurPackeges from "../../Components/OurPackege/OurPackeges";
 
 const Home = () => {
     useEffect(()=>{
@@ -12,6 +13,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Brands></Brands>
+            <OurPackeges></OurPackeges>
         </div>
     );
 };
